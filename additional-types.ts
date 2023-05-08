d8053333223bf933ee2fe4a81d4cf22fbed41d87
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-05-08 22:42:27 +0300

    add: types 2

--- additional-types.ts
+++ additional-types.ts
@@ -5,3 +5,19 @@ let test: any = 123;
 test = "124";
 
 test.map();
+
+//Unknown
+
+let test2: unknown = 1231;
+
+let string123: string = test;
+
+//void
+
+function f(arg: string, next: string): void {
+  // return 1;
+}
+
+const f2 = (arg: string, next: string): void => {
+  // return 1
+};
