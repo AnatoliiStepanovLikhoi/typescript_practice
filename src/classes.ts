40fabd6adce63d1d357988e9d9238ec63e11d852
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-06-20 22:21:05 +0300

    add: classes static

--- src/classes.ts
+++ src/classes.ts
@@ -47,3 +47,31 @@ stoneHouse.addTenant("Anton");
 stoneHouse.addTenant("Nikita");
 
 stoneHouse.showTenants();
+
+//Static methods
+
+class UseStatic {
+  private static count = 0;
+
+  constructor() {
+    UseStatic.count += 1;
+  }
+
+  public static isStaticMethod() {
+    console.log("I`m static");
+  }
+
+  public showCount() {
+    console.log(UseStatic.count);
+  }
+}
+
+const obj1 = new UseStatic();
+const obj2 = new UseStatic();
+const obj3 = new UseStatic();
+
+obj1.showCount();
+obj2.showCount();
+obj3.showCount();
+
+UseStatic.isStaticMethod();
