16693927cfe623249208acc32bcfdb5bfe875909
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-06-24 10:57:49 +0300

    add: generic functions

--- src/generics.ts
+++ src/generics.ts
@@ -13,3 +13,43 @@ promise.then((data) => {});
 
   promiseFoo().then((data) => {});
 })();
+
+//Generic function
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign({}, objA, objB);
+}
+
+const toMerge1 = {
+  name: "Max",
+};
+
+const toMerge2 = {
+  age: 21,
+};
+
+const merged = merge(toMerge1, toMerge2);
+
+merged.name;
+
+interface ILength {
+  lenght: number;
+}
+
+function getLength<T extends ILength>(str: T): number {
+  return str.lenght;
+}
+
+const obj5 = {
+  lenght: 10,
+};
+
+console.log(getLength(obj5));
+
+//keyof
+
+function extractValue<T extends object, U extends keyof T>(obj: T, key: U) {
+  return obj[key];
+}
+
+const field = extractValue({ name: "Anton" }, "name");
