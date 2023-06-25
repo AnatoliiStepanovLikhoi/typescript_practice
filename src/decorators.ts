a58e9131e898c3be61195fdb9b04bf2135a33f39
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-06-25 15:16:03 +0300

    add: complicated decorators

--- src/decorators.ts
+++ src/decorators.ts
@@ -24,3 +24,41 @@ class Controller1 {
 const controller = new Controller1();
 
 console.log("Is modify?", controller.modify);
+
+//complicated decorators
+interface IDecoration {
+  parent: string;
+  template: string;
+}
+
+function ControllerDecoration(config: IDecoration) {
+  return function <T extends { new (...arg: any[]): { content: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      private element: HTMLElement;
+      private parent: HTMLElement;
+
+      constructor(...args: any[]) {
+        super(...args);
+
+        this.parent = document.getElementById(config.parent)!;
+        this.element = document.createElement(config.template);
+
+        this.element.innerHTML = this.content;
+        this.parent.appendChild(this.element);
+      }
+    };
+  };
+}
+
+@ControllerDecoration({
+  parent: "app",
+  template: "H1",
+})
+class Controller {
+  public content = "My controller";
+}
+
+const controller2 = new Controller();
+const controller3 = new Controller();
