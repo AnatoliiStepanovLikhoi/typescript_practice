a9c9b4ed1f71eec0d61608068980e3511012c3fd
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-09-12 22:46:12 +0300

    add: enums alternative

--- src/demo.ts
+++ src/demo.ts
@@ -1,4 +1,4 @@
-const a = JSON.parse("{a: 1}");
+const a = JSON.parse('{"a": 1}');
 
 if (typeof a === "object" && a != null && "a" in a) {
   a.a;
@@ -27,3 +27,27 @@ const newArr = arr111.filter(Boolean);
 const newArr2 = arr111.filter((e): e is number => {
   return !!e;
 });
+
+// enums
+
+enum UserRolesEnum {
+  User,
+  Admin,
+  Staff,
+}
+
+if (UserRolesEnum.User) {
+  console.log("this will never get hit");
+}
+
+const UserRoles = ["User", "Admin", "Staff"] as const;
+
+type UserRole = (typeof UserRoles)[number];
+
+type User12 = {
+  role: UserRole;
+};
+
+const Dropdown = () => {
+  const data = UserRoles.map((role) => ({ value: role, label: role }));
+};
